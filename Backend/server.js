6bd493e265bf7c10b6306bbbb6722ac776f7ce53
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-09-06 18:48:24 +0200

    Commit server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@
 const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
-const port = process.env.PORT || 8000;
+const port = process.env.APPPORT || 8000;
 server.listen( port , () => {
     console.log('Congrats , the server is listening on port ', port);
 });
