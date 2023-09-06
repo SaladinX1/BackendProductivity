cf814bd730c7ae695cea0fc2eccb53d0a42c663e
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-09-06 14:53:07 +0200

    Commit path render server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 
 
-const app = require('../../../../../app');
+const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 server.listen(process.env.PORT || 8000 , () => {
