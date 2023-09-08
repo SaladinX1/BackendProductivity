6b63cd544e010c546566d7caf66a0a4f8d7b75ad
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-09-08 16:43:39 +0200

    path images post multer controller addpost 9 + server.js modification

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 
-
-const express = require('express');
 const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 const port = process.env.APPPORT || 8000;
 
-app.use(express());
 
 server.listen( port , () => {
     console.log('Congrats , the server is listening on port ', port);
