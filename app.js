db576b3c46eaa83925a6362dbb1faa04873fc940
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-09-09 15:34:01 +0200

    path images app.js 17 + post.js controller addpost

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({
 
 
 
-//app.use('/', express.static(path.join(__dirname, 'fdfd')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 app.use('/api', userRoutes);
